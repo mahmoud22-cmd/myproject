feb722ad614659ecb395e3406df679f8f47ab227
mahmoud22-cmd <mvhmodkjr@gmail.com>
2024-09-04 12:16:24 +0300

    WIth message for the grade

--- distribution.dart
+++ distribution.dart
@@ -9,10 +9,20 @@ void main() {
   for (int i = 0; i < 1500; i++) {
     String name = "Student ${i + 1}";
     int age = random.nextInt(30); // value is >=0 & 30
-    double grade = random.nextDouble();
+    double grade = random.nextDouble() * 30 + 70;
 
     students.add(Student(name: name, age: age, grade: grade));
     print("# Name: $name");
+    print("# Grade: $grade%");
+    if (grade >= 85) {
+      print("Excellent");
+    } else if (grade < 85 && grade >= 70) {
+      print("Very Good");
+    } else if (grade < 70 && grade >= 60) {
+      print("Good");
+    } else {}
+    ;
+
     for (int x = 0; x < 50; x++) {
       String title = "University ${x + 1}";
       String location = "City ${x + 1}";
@@ -30,5 +40,6 @@ void main() {
         continue;
       }
     }
+    print("");
   }
 }
